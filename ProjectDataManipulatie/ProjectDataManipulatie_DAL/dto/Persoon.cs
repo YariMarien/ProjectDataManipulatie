8e9f6f5190e11ee4ed7af7606200e09d4721dedc
Yari Mariën <r0864345@student.thomasmore.be>
2021-05-23 21:35:01 +0200

    Data transfer object created

---
+++ ProjectDataManipulatie/ProjectDataManipulatie_DAL/dto/Persoon.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectDataManipulatie_DAL.dto
+{
+    public class Persoon
+    {
+        public int Id { get; set; }
+        public string fullName { get; set; }
+        public string email { get; set; }
+        public string clubNaam { get; set; }
+        public string geslacht { get; set; }
+        public string borstNummer { get; set; }
+        public string password { get; set; }
+        public DateTime geboorteDatum { get; set; }
+    }
+}
