376350462c1b23efe583d49eaff93f7e4319c5d4
Yari Mariën <r0864345@student.thomasmore.be>
2021-05-23 21:40:49 +0200

    Added Unit testing project + 5 validation tests

---
+++ ProjectDataManipulatie/ProjectDatamanipulatie_Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ProjectDatamanipulatie_Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("ProjectDatamanipulatie_Test")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("73730d23-60b1-49f4-90e8-e23d78f954e8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ProjectDataManipulatie/ProjectDatamanipulatie_Test/UnitTest1.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjectDataManipulatie_DAL;
+using System;
+
+namespace ProjectDatamanipulatie_Test
+{
+    [TestClass]
+    public class ValidPersonTest
+    {
+        [TestMethod]
+        public void TestPersonValidation_Invalid()
+        {
+            Persoon p = new Persoon();
+            bool geldig = p.IsGeldig();
+            Assert.AreEqual(false, geldig);
+        }
+        //Niet de beste oplossing om de errors te testen.
+        //Betere manier is dat ik met een enum zou werken.
+        [TestMethod]
+        public void TestPersonValidation_EmptyEmail()
+        {
+            Persoon p = new Persoon();
+
+
+            bool isGeldig = p.IsGeldig();
+
+            Assert.AreEqual(false, isGeldig);
+            Assert.IsTrue(p.Error.Contains("Email is verplicht in te vullen."));
+
+        }
+
+        [TestMethod]
+        public void TestPersonValidation_InvalidEmail()
+        {
+            Persoon p = new Persoon() {
+            email = "abc"
+            };
+
+
+            bool isGeldig = p.IsGeldig();
+
+
+            Assert.AreEqual(false, isGeldig);
+            Assert.IsTrue(p.Error.Contains("Ongeldig email adres."));
+
+        }
+        [TestMethod]
+        public void TestPersonValidation_InvalidBirthDate()
+        {
+            Persoon p = new Persoon()
+            {
+                geboorteDatum = DateTime.Now
+            };
+
+
+            bool isGeldig = p.IsGeldig();
+
+
+            Assert.AreEqual(false, isGeldig);
+            Assert.IsTrue(p.Error.Contains("Je moet minstens 6 jaar oud zijn."));
+
+        }
+        [TestMethod]
+        public void TestPersonValidation_ValidPerson()
+        {
+            Persoon p = new Persoon()
+            {
+                geboorteDatum = new DateTime(2010, 01, 01),
+                email = "geldig.mailadres@telenet.be"
+            };
+
+
+            bool isGeldig = p.IsGeldig();
+
+
+            Assert.AreEqual(true, isGeldig);
+            Assert.IsTrue(string.IsNullOrEmpty(p.Error));
+
+        }
+    }
+}
